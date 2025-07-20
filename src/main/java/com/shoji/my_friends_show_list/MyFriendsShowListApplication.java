d47bb60496c879435671b484beb3f5673832e0ff
MKShoji <matheusshoji@outlook.com>
2025-07-20 12:32:52 -0300

    Initial commit

---
+++ src/main/java/com/shoji/my_friends_show_list/MyFriendsShowListApplication.java
@@ -0,0 +1,13 @@
+package com.shoji.my_friends_show_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFriendsShowListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFriendsShowListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoji/my_friends_show_list/MyFriendsShowListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoji.my_friends_show_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFriendsShowListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
